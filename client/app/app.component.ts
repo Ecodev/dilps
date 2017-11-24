605e6315b80cdb0a9a17673d10e36e41d4cad1aa
Adrien Crivelli <adrien.crivelli@gmail.com>
2017-11-24 17:09:50 +0900

    First Apollo query

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+// We use the gql tag to parse our query string into a query document
+const Users = gql`
+  query Users {
+    users {
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'DILPS';
+    users: any;
+
+    constructor(private apollo: Apollo) {
+    }
+
+    ngOnInit() {
+        this.apollo.watchQuery<any>({
+            query: Users,
+        })
+            .valueChanges
+            .subscribe(({data}) => {
+                this.users = data.users;
+            });
+    }
 }
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule } from 'apollo-angular';
+import { HttpLinkModule } from 'apollo-angular-link-http';
+import { Apollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule, // provides HttpClient for HttpLink
+        ApolloModule,
+        HttpLinkModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+    constructor(apollo: Apollo, httpLink: HttpLink) {
+        apollo.create({
+            link: httpLink.create({uri: '/graphql'}),
+            cache: new InMemoryCache(),
+        });
+    }
+
+}
