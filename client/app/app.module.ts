40fda7367063d9649b1cce701d69815fd9163c7e
Adrien Crivelli <adrien.crivelli@gmail.com>
2017-11-24 17:38:48 +0900

    First material

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpLinkModule } from 'apollo-angular-link-http';
 import { Apollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -18,6 +20,10 @@ import { AppComponent } from './app.component';
         HttpClientModule, // provides HttpClient for HttpLink
         ApolloModule,
         HttpLinkModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatIconModule
     ],
     providers: [],
     bootstrap: [AppComponent],
