65d171ba6adc39f7d8d096fbca1f81b86f5553e1
Samuel Baptista <samuel.baptista@ecodev.ch>
2017-12-04 19:24:53 +0100

    Display a first route

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -22,6 +22,10 @@ export const routes: Routes = [
                 path: '',
                 component: UserComponent,
             },
+            {
+                path: 'user',
+                component: UserComponent,
+            },
         ],
     },
 
