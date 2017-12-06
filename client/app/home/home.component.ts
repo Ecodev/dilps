e2dd7eed7014b2d12e0b1049e9fb03bf0a32796c
Samuel Baptista <samuel.baptista@ecodev.ch>
2017-12-06 17:37:36 +0100

    Simplify menu
    As Dilps will never be used in mobile context, no need to manage open/closed status, mode (side /over).
    
    Wait and see for minimized status, but a tooltip could be enougth

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -1,43 +1,18 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { SidemenuService } from '../shared/services/sidemenu.service';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../shared/services/theme.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    providers: [
-        // NavigationService,
-        SidemenuService,
-    ],
+    providers: [],
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
-    /**
-     * Fix for refresh sidenav resize
-     * Todo : Remove when https://github.com/angular/material2/issues/6743 is fixed
-     * Todo : Remove ngAfterViewInit function
-     * Todo : Removce #navContainer in template
-     */
-    @ViewChild('navContainer') private navContainer;
-
-    constructor(public sidemenuSvc: SidemenuService, private themeSvc: ThemeService) {
+    constructor(private themeSvc: ThemeService) {
     }
 
     ngOnInit() {
-    }
-
-    /**
-     * Fix for refresh sidenav resize
-     * Todo : Remove when https://github.com/angular/material2/issues/6743 is fixed
-     * Todo : Remove @ViewChild('navContainer') private navContainer too
-     * Todo : Remove #navContainer in template
-     */
-    ngAfterViewInit() {
-        this.navContainer._ngZone.onMicrotaskEmpty.subscribe(() => {
-            this.navContainer._updateContentMargins();
-            this.navContainer._changeDetectorRef.markForCheck();
-        });
     }
 
     toggleTheme() {
