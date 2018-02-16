a65bf97375a7e1191a4d37c6f2f3821d95b6a458
Samuel Baptista <samuel.baptista@ecodev.ch>
2018-02-16 10:54:40 +0100

    Add autocompletion to app-thesaurus #5464

--- client/app/shared/components/thesaurus/thesaurus.component.ts
+++ client/app/shared/components/thesaurus/thesaurus.component.ts
@@ -1,6 +1,14 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
-import { MatChipInputEvent } from '@angular/material';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { MatAutocompleteTrigger, MatChipInputEvent } from '@angular/material';
+import { ENTER } from '@angular/cdk/keycodes';
+import { FormControl } from '@angular/forms';
+import { IncrementSubject } from '../../services/increment-subject';
+import { QueryRef } from 'apollo-angular';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Literal } from '../../types';
+import { filter, map, sampleTime } from 'rxjs/operators';
+import { isObject } from 'lodash';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'app-thesaurus',
@@ -9,49 +17,163 @@ import { COMMA, ENTER } from '@angular/cdk/keycodes';
 })
 export class ThesaurusComponent implements OnInit, OnChanges {
 
+    @ViewChild(MatAutocompleteTrigger) public autocomplete: MatAutocompleteTrigger;
+    @ViewChild('input') input;
+
+    @Input() service;
     @Input() placeholder;
-    @Input() model: string[] = [];
-    @Output() modelChange = new EventEmitter();
+    @Input() model: any[] = [];
+
+    public formCtrl: FormControl = new FormControl();
+
+    private queryRef: QueryRef<any>;
+
+    public loading = false;
+
+    /**
+     * Default page size
+     * @type {number}
+     */
+    private pageSize = 10;
+
+    /**
+     * Init search options
+     * @type {IncrementSubject}
+     */
+    private options: IncrementSubject;
+
+    /**
+     * Number of items not shown in result list
+     * Shows a message after list if positive
+     */
+    public moreNbItems = 0;
+
+    /**
+     * Filter options debounced.
+     * Used by the query
+     */
+    private optionsFiltered: BehaviorSubject<Literal>;
+
+    public suggestions: Literal[] = [];
+    public suggestionsObs: Observable<any>;
 
     constructor() {
     }
 
     public separatorKeysCodes = [
         ENTER,
-        COMMA,
     ];
 
+    public addOnBlur = true;
+
     ngOnInit() {
+
+        const options = {
+            filters: {
+                search: null,
+            },
+            pagination: {
+                pageIndex: 0,
+                pageSize: 10,
+            },
+        };
+
+        this.options = new IncrementSubject(options);
+        this.optionsFiltered = new BehaviorSubject<Literal>(options);
+
+        // Debounce search...
+        // ...and filter only string search terms (when an item is selected, the value is an object)
+        this.options.pipe(sampleTime(400), filter(val => {
+            return !isObject(val.filters.search); // prevent to emit when value is an object
+        })).subscribe(data => {
+            this.optionsFiltered.next(data);
+        });
     }
 
     ngOnChanges() {
-        this.model = this.model.map((v: any) => v.name ? v.name : v);
+        for (let i = 0; i < this.model.length; i++) {
+            this.model[i] = this.model[i].name;
+        }
     }
 
-    add(event: MatChipInputEvent): void {
-        const input = event.input;
-        const term = event.value.trim();
+    public onFocus() {
+        this.startSearch();
+    }
 
+    open() {
+        this.autocomplete.openPanel();
+    }
+
+    public startSearch() {
+
+        /**
+         * Start search only once
+         */
+        if (this.queryRef) {
+            return;
+        }
+
+        this.loading = true;
+
+        this.optionsFiltered.subscribe((data) => {
+            this.addOnBlur = false;
+            if (data.filters.search) {
+                this.loading = true;
+            }
+        });
+
+        // Init query
+        this.queryRef = this.service.watchAll(this.optionsFiltered);
+
+        // When query results arrive, start loading, and count items
+        this.queryRef.valueChanges.subscribe((data: any) => {
+            this.loading = false;
+
+            const nbTotal = data.length;
+            const nbListed = Math.min(data.length, this.pageSize);
+            this.moreNbItems = nbTotal - nbListed;
+        });
+
+        this.suggestionsObs = this.queryRef.valueChanges.pipe(map((data: any) => {
+            this.suggestions = data.items.filter(item => this.model.findIndex(term => term === item.name));
+            if (this.suggestions.length === 0) {
+                this.addOnBlur = true;
+            }
+            return this.suggestions;
+        }));
+    }
+
+    private addTerm(term: string) {
+        this.autocomplete.closePanel();
         const indexOf = this.model.findIndex(item => item === term);
         if (term && indexOf === -1) {
             this.model.push(term.trim());
         }
+    }
 
+    public addChip(event: MatChipInputEvent): void {
+        const term = event.value.trim();
+        this.addTerm(term);
+        const input = event.input;
         if (input) {
             input.value = '';
         }
-
-        this.modelChange.emit(this.model);
     }
 
-    remove(term: any): void {
+    public selectSuggestion(event) {
+        this.addTerm(event.option.value.name);
+    }
+
+    public removeChip(term: any): void {
         const index = this.model.indexOf(term);
 
         if (index >= 0) {
             this.model.splice(index, 1);
         }
-
-        this.modelChange.emit(this.model);
     }
 
+    public search(ev) {
+        const options = {filters: {search: ev}};
+        this.options.patch(options);
+    }
 }
