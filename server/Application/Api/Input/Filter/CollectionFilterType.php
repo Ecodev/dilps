0ffd5f6ea366d0d0f2e82b0811c7d065b3765b40
Samuel Baptista <samuel.baptista@ecodev.ch>
2018-02-28 11:37:34 +0100

    Allow to filter collections by owners and isSource #5491

---
+++ server/Application/Api/Input/Filter/CollectionFilterType.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Api\Input\Filter;
+
+use GraphQL\Type\Definition\InputObjectType;
+
+class CollectionFilterType extends InputObjectType
+{
+    public function __construct()
+    {
+        $config = [
+            'fields' => function (): array {
+                return [
+                    'creators' => [
+                        'type' => self::listOf(self::nonNull(self::id())),
+                    ],
+                    'isSource' => [
+                        'type' => self::boolean(),
+                    ],
+                ];
+            },
+        ];
+
+        parent::__construct($config);
+    }
+}
--- server/Application/Repository/CollectionRepository.php
+++ server/Application/Repository/CollectionRepository.php
@@ -4,6 +4,30 @@ declare(strict_types=1);
 
 namespace Application\Repository;
 
+use Doctrine\ORM\QueryBuilder;
+
 class CollectionRepository extends AbstractRepository
 {
+    public function getFindAllQuery(array $filters = []): QueryBuilder
+    {
+        $qb = $this->createQueryBuilder('collection');
+
+        if (isset($filters['isSource'])) {
+            $qb->andWhere('collection.isSource = :isSource');
+            $qb->setParameter('isSource', $filters['isSource']);
+        }
+
+        if (isset($filters['creators'])) {
+            if (\count($filters['creators']) > 0) {
+                $qb->andWhere('collection.creator IN (:creators)');
+                $qb->setParameter('creators', $filters['creators']);
+            } else {
+                $qb->andWhere('collection.creator IS NULL');
+            }
+        }
+
+        $qb->addOrderBy('collection.id');
+
+        return $qb;
+    }
 }
