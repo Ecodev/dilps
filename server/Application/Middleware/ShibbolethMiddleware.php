23eafd08edacc086c2ca675e4df84a5681548827
Luca Guindani <luca.guindani@gmail.com>
2018-08-13 15:31:10 +0200

    Added redirect in ShibbolethMiddleware if a Moodle parameter is found

--- server/Application/Middleware/ShibbolethMiddleware.php
+++ server/Application/Middleware/ShibbolethMiddleware.php
@@ -24,10 +24,20 @@ class ShibbolethMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // Redirect to quizz url if quizz parameter is found in the query params
-        $quizzUrl = array_key_exists('quizz', $request->getQueryParams()) ?
-            'quizz;cards=' . $request->getQueryParams()['quizz'] . ';nav=0' : '';
+        // Redirect to specific moodle url if moodle parameter found in the query params
+        if (array_key_exists('moodle', $request->getQueryParams())) {
+            $moodleUrl = 'https://moodle.unil.ch/filter/uniltools/redirmod.php?id=' . $request->getQueryParams()['moodle'];
 
-        return new RedirectResponse('/' . $quizzUrl, 302);
+            return new RedirectResponse($moodleUrl, 302);
+        }
+
+        // Redirect to quizz url if quizz parameter is found in the query params
+        if (array_key_exists('quizz', $request->getQueryParams())) {
+            $quizzUrl = '/quizz;cards=' . $request->getQueryParams()['quizz'] . ';nav=0';
+
+            return new RedirectResponse($quizzUrl, 302);
+        }
+
+        return new RedirectResponse('/', 302);
     }
 }
