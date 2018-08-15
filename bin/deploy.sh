38b93e32c735e10e8f57468d5ae35a1c7d418e38
Luca Guindani <luca.guindani@gmail.com>
2018-08-15 10:22:45 +0200

    Modified build & deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# To update & rebuild Dilps, launch this script from the project root directory
+
 # Make a dump of the database
-echo "Dumping database..."
-php ./dump-data.php $(date +%Y%m%d%H%M%S).db.backup.sql.gz
+echo "********************* Dumping database..."
+php $PWD/bin/dump-data.php $(date +%Y%m%d%H%M%S).db.backup.sql.gz
 
 # Update project from master branch
-echo "Updating project files..."
+echo "********************* Updating project files..."
 git pull origin master
 
 # Rebuild project
-sh ./build.sh
+sh $PWD/bin/build.sh
